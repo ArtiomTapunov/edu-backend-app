7221e503abd16dba9bd94dcff5ae46fa6200f77a
Artiom <artiom.tapunov@gmail.com>
2020-05-24 15:17:10 +0300

    Domain Objects added
    User object.

---
+++ EduCoreApp/ECA.DomainObjects/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ECA.DomainObjects
+{
+    public class User
+    {
+        public long UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime LastLoggedIn { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; }
+    }
+}
