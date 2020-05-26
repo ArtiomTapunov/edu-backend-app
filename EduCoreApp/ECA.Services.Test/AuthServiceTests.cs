0b081e4cf32f7c21507a465f5310948a72cadb50
Artiom <artiom.tapunov@gmail.com>
2020-05-27 00:28:12 +0300

    Login on success test added

--- EduCoreApp/ECA.Services.Test/AuthServiceTests.cs
+++ EduCoreApp/ECA.Services.Test/AuthServiceTests.cs
@@ -1,3 +1,6 @@
+using ECA.Services.Implemintations;
+using ECA.Services.Test.Mocks;
+using ECA.Services.ViewModels;
 using NUnit.Framework;
 
 namespace ECA.Services.Test
@@ -10,9 +13,24 @@ namespace ECA.Services.Test
         }
 
         [Test]
-        public void Test1()
+        public void AuthService_Login_Success()
         {
-            Assert.Pass();
+            AuthService authService = GetService();
+            var loginModel = new LoginViewModel
+            {
+                Email = "john.doe@example.com",
+                Password = "123456aa"
+            };
+
+            var result = authService.Login(loginModel);
+            Assert.IsNotNull(result);
+        }
+
+        private static AuthService GetService()
+        {
+            var mockRepo = new MockUserRepository();
+            var authService = new AuthService(mockRepo);
+            return authService;
         }
     }
 }
