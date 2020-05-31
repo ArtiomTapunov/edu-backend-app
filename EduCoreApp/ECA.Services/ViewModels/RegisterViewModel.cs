e3bcf0c1286a784b3de7c79449f3d659df163b16
Artiom <artiom.tapunov@gmail.com>
2020-05-31 17:02:48 +0300

    Register view model added

---
+++ EduCoreApp/ECA.Services/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECA.Services.ViewModels
+{
+    public class RegisterViewModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string PasswordConfirmation { get; set; }
+
+        public bool HasAcceptedTerms { get; set; }
+    }
+}
