0485d237bae8872bda7750b7d09b6a6afd754409
Artiom <artiom.tapunov@gmail.com>
2020-05-31 17:07:22 +0300

    User repository added

---
+++ EduCoreApp/ECA.DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,51 @@
+﻿using ECA.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ECA.DataAccess.Repositories
+{
+    public class UserRepository : IRepository<User>
+    {
+        private readonly List<User> Users;
+        public UserRepository()
+        {
+            Users = new List<User> {
+                new User {
+                    UserId = "1",
+                    DateCreated = DateTime.Now,
+                    Email = "john.doe@example.com",
+                    FirstName = "John",
+                    LastName = "Doe",
+                    LastLoggedIn = DateTime.Now,
+                    PasswordHash = "1000:CgToWlPECE5TKFrq5jm5KhFKA6TT8/nf:AHnvXvVr2OzrOcF7AuIDqxyJIJg=",
+                    Role = "Admin"
+                }
+            };
+        }
+
+        public IQueryable<User> GetAll()
+        {
+            return Users.AsQueryable();
+        }
+
+        public void Insert(User entry)
+        {
+            if (entry == null)
+                throw new ArgumentNullException(nameof(entry));
+            Users.Add(entry);
+        }
+
+        public int SaveChanges()
+        {
+            return 0;
+        }
+
+        public IQueryable<User> Where(Expression<Func<User, bool>> predicate)
+        {
+            return Users.Where(predicate.Compile()).AsQueryable();
+        }
+    }
+}
