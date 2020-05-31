7ba13567bf78d97b71365aeea247dd0848c38ee9
Artiom <artiom.tapunov@gmail.com>
2020-05-31 17:02:07 +0300

    Weak password exception added

---
+++ EduCoreApp/ECA.Services/Errors/WeakPasswordException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ECA.Services.Errors
+{
+    public class WeakPasswordException : Exception
+    {
+        public WeakPasswordException() { }
+
+        public WeakPasswordException(string message) : base(message) { }
+
+        public WeakPasswordException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected WeakPasswordException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
