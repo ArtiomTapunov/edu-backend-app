e3ce439c22339acf0f0dd899045811f03d04996f
Artiom <artiom.tapunov@gmail.com>
2020-05-31 17:01:28 +0300

    Password mismatch exception added

---
+++ EduCoreApp/ECA.Services/Errors/PasswordMismatchException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ECA.Services.Errors
+{
+    public class PasswordMismatchException : Exception
+    {
+        public PasswordMismatchException() { }
+
+        public PasswordMismatchException(string message) : base(message) { }
+
+        public PasswordMismatchException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected PasswordMismatchException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
