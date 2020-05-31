8d0d874fe8c313632e66f6b2c07402df2fd53bda
Artiom <artiom.tapunov@gmail.com>
2020-05-31 17:01:02 +0300

    Terms not accepted exception added

---
+++ EduCoreApp/ECA.Services/Errors/TermsNotAcceptedException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ECA.Services.Errors
+{
+    public class TermsNotAcceptedException : Exception
+    {
+        public TermsNotAcceptedException() { }
+
+        public TermsNotAcceptedException(string message) : base(message) { }
+
+        public TermsNotAcceptedException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected TermsNotAcceptedException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
